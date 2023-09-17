b48774529deb3e05cb3035be29b2168748681320
elina <papadelina145@gmail.com>
2023-09-17 21:14:19 +0300

    Add new messages

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Message Board', messages: messages });
 });
 
+// Get new message form
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Message Board', messages: messages });
+});
+
+router.post('/new', function(req, res) {
+  messages.push({text: req.body.message, user: req.body.user, added: new Date()});
+  res.redirect('/');
+});
+
 module.exports = router;
