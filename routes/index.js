1e9e4471df5a2cdce10d7f953a2321db6fc73f0a
elina <papadelina145@gmail.com>
2023-09-17 20:53:26 +0300

    Display messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amanda",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
 
 module.exports = router;
